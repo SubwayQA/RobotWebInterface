2a18ae2f05c826a482a73e049aa3cf8cb644bae7
Andrey Teplyakov <teplyakov_a@subway.com>
2017-03-28 08:59:53 -0400

    v3 Upload HEX update. Code refactoring

--- show_bar.sh
+++ show_bar.sh
@@ -5,7 +5,6 @@ export HOME=/home/pi/
 
 path="/home/pi/uploads"
 old_string="EndProlog"
-#new_string="EndProlog\n\n%%BeginSetup\n << /PageSize [842 595] /Orientation 3 >> setpagedevice\n%%EndSetup\n"
 new_string="EndProlog\n\n%%BeginSetup\n << /PageSize [842 595] /Orientation 3 >> setpagedevice\n%%EndSetup\n"
 
 rm -f $path/bar_code.ps
@@ -16,7 +15,6 @@ xset dpms force on
 
 barcode -b $1 -g 800x400 -o $path/bar_code.ps
 sed -i "s;$old_string;$new_string;g" $path/bar_code.ps
-#barcode -b $1 -o $path/bar_code.ps
 ps2pdf $path/bar_code.ps $path/bar_code.pdf
 pdftoppm -png $path/bar_code.pdf $path/bar_code
 feh -F $path/bar_code-1.png > /dev/null 2>&1 &
